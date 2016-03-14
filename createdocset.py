2b3b629f4a6ab9a1997a7dfe9ff26d9d9d904f1e
Björn Ritzl <bjorn.ritzl@king.com>
2016-03-14 11:19:03 +0100

    Cleanup after running script

--- createdocset.py
+++ createdocset.py
@@ -36,6 +36,7 @@ ref_path = os.path.join(documents_path, "ref")
 
 
 DOC_ZIP = "ref-doc.zip"
+JSON_PATH = "json"
 
 def get_defold_sha1():
 	info_url = "http://d.defold.com/stable/info.json"
@@ -50,14 +51,21 @@ def get_ref_doc():
 		os.remove(DOC_ZIP)
 	urllib.urlretrieve("http://d.defold.com/archive/" + sha1 + "/engine/share/ref-doc.zip", DOC_ZIP)
 
-def unzip_ref_doc(path):
-	if os.path.exists(path):
-		shutil.rmtree(path)
+def cleanup():
+	if os.path.exists(JSON_PATH):
+		shutil.rmtree(JSON_PATH)
+	if os.path.exists(DOC_ZIP):
+		os.remove(DOC_ZIP)
+
+def unzip_ref_doc():
+	if os.path.exists(JSON_PATH):
+		shutil.rmtree(JSON_PATH)
 
 	with zipfile.ZipFile(DOC_ZIP) as zf:
-		zf.extractall(path)
+		zf.extractall(JSON_PATH)
 
-def create_paths():
+
+def create_docset():
 	# create all paths
 	if os.path.exists(ref_path):
 		shutil.rmtree(ref_path)
@@ -70,64 +78,61 @@ def create_paths():
 	# copy icon
 	shutil.copyfile("icon.png", os.path.join(docset_path, "icon.png"))
 
+	# create sqlite db
+	with sqlite3.connect(os.path.join(resources_path, "docSet.dsidx")) as db:
+		cursor = db.cursor()
+
+		try: cursor.execute('DROP TABLE searchIndex;')
+		except: pass
+
+		# create db table
+		cursor.execute('CREATE TABLE searchIndex(id INTEGER PRIMARY KEY, name TEXT, type TEXT, path TEXT);')
+		# make sure duplicates are ignored
+		#cursor.execute('CREATE UNIQUE INDEX anchor ON searchIndex (name, type, path);')
+
+		index_html = ""
+		for root, dir, files in os.walk(JSON_PATH):
+			for file in files:
+				with open(os.path.join(root, file), "r") as fh:
+					if file.endswith(".json"):
+						class_name = file.replace("_doc.json", "")
+						class_path = class_name + ".html"
+						class_doc = ""
+						for element in json.load(fh)["elements"]:
+							function_name = element["name"]
+							function_path = class_path + "#" + function_name
+							class_doc = class_doc + "<h1><a name='" + function_name + "'>" + function_name + "</a></h1>"
+							class_doc = class_doc + "<p>" + element["brief"] + "</p>"
+							if element["description"] != "":
+								class_doc = class_doc + "<p>" + element["description"] + "</p>"
+							if len(element["parameters"]) > 0:
+								class_doc = class_doc + "<h3>PARAMETERS</h3>"
+								for parameter in element["parameters"]:
+									class_doc = class_doc + "<p>" + parameter["name"] + " - "  + parameter["doc"] + "</p>"
+							if element["return_"] != "":
+								class_doc = class_doc + "<h3>RETURN</h3>"
+								class_doc = class_doc + "<p>" + element["return_"] + "</p>"
+							if element["examples"] != "":
+								class_doc = class_doc + "<h3>EXAMPLES</h3>"
+								class_doc = class_doc + "<p>" + element["examples"] + "</p>"
+
+							type = "Function"
+							if element["type"] == "VARIABLE":
+								type = "Variable"
+							elif element["type"] == "MESSAGE":
+								type = "Variable"
+							cursor.execute('INSERT OR IGNORE INTO searchIndex(name, type, path) VALUES (?,?,?)', (function_name, 'Function', "ref/" + function_path))
+
+						index_html = index_html + "<a href='ref/" + class_path + "'>" + class_name + "</a></br>"
+						with open(os.path.join(ref_path, class_path), "w") as out:
+							out.write(class_doc)
+
+						cursor.execute('INSERT OR IGNORE INTO searchIndex(name, type, path) VALUES (?,?,?)', (class_name, 'Module', "ref/" + class_path))
+
+		with open(os.path.join(documents_path, "index.html"), "w") as out:
+			out.write(index_html)
+
 get_ref_doc()
-
-JSON_PATH = "json"
-unzip_ref_doc(JSON_PATH)
-create_paths()
-
-
-
-# create sqlite db
-with sqlite3.connect(os.path.join(resources_path, "docSet.dsidx")) as db:
-	cursor = db.cursor()
-
-	try: cursor.execute('DROP TABLE searchIndex;')
-	except: pass
-
-	# create db table
-	cursor.execute('CREATE TABLE searchIndex(id INTEGER PRIMARY KEY, name TEXT, type TEXT, path TEXT);')
-	# make sure duplicates are ignored
-	#cursor.execute('CREATE UNIQUE INDEX anchor ON searchIndex (name, type, path);')
-
-	index_html = ""
-	for root, dir, files in os.walk(JSON_PATH):
-		for file in files:
-			with open(os.path.join(root, file), "r") as fh:
-				if file.endswith(".json"):
-					class_name = file.replace("_doc.json", "")
-					class_path = class_name + ".html"
-					class_doc = ""
-					for element in json.load(fh)["elements"]:
-						function_name = element["name"]
-						function_path = class_path + "#" + function_name
-						class_doc = class_doc + "<h1><a name='" + function_name + "'>" + function_name + "</a></h1>"
-						class_doc = class_doc + "<p>" + element["brief"] + "</p>"
-						if element["description"] != "":
-							class_doc = class_doc + "<p>" + element["description"] + "</p>"
-						if len(element["parameters"]) > 0:
-							class_doc = class_doc + "<h3>PARAMETERS</h3>"
-							for parameter in element["parameters"]:
-								class_doc = class_doc + "<p>" + parameter["name"] + " - "  + parameter["doc"] + "</p>"
-						if element["return_"] != "":
-							class_doc = class_doc + "<h3>RETURN</h3>"
-							class_doc = class_doc + "<p>" + element["return_"] + "</p>"
-						if element["examples"] != "":
-							class_doc = class_doc + "<h3>EXAMPLES</h3>"
-							class_doc = class_doc + "<p>" + element["examples"] + "</p>"
-
-						type = "Function"
-						if element["type"] == "VARIABLE":
-							type = "Variable"
-						elif element["type"] == "MESSAGE":
-							type = "Variable"
-						cursor.execute('INSERT OR IGNORE INTO searchIndex(name, type, path) VALUES (?,?,?)', (function_name, 'Function', "ref/" + function_path))
-
-					index_html = index_html + "<a href='ref/" + class_path + "'>" + class_name + "</a></br>"
-					with open(os.path.join(ref_path, class_path), "w") as out:
-						out.write(class_doc)
-
-					cursor.execute('INSERT OR IGNORE INTO searchIndex(name, type, path) VALUES (?,?,?)', (class_name, 'Module', "ref/" + class_path))
-
-	with open(os.path.join(documents_path, "index.html"), "w") as out:
-		out.write(index_html)
+unzip_ref_doc()
+create_docset()
+cleanup()
