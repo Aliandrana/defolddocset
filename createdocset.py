3419d81cc923385a71dbfa1a637be5b80d6acb42
Björn Ritzl <bjorn.ritzl@king.com>
2016-03-14 11:10:17 +0100

    Updated generator to use the doc ref that is bundled with every release

--- createdocset.py
+++ createdocset.py
@@ -5,7 +5,8 @@ import sqlite3
 import shutil
 import urllib
 import urllib2
-from bs4 import BeautifulSoup, NavigableString, Tag
+import json
+import zipfile
 
 
 INFO_PLIST = """<!DOCTYPE plist SYSTEM "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
@@ -26,98 +27,107 @@ INFO_PLIST = """<!DOCTYPE plist SYSTEM "http://www.apple.com/DTDs/PropertyList-1
 """
 
 
-
 # path definitions
 docset_path = "defold.docset"
 contents_path = os.path.join(docset_path, "Contents")
 resources_path = os.path.join(contents_path, "Resources")
 documents_path = os.path.join(resources_path, "Documents")
-index_path = os.path.join(documents_path, "index.html")
 ref_path = os.path.join(documents_path, "ref")
-db_path = os.path.join(resources_path, "docSet.dsidx")
 
 
-def parse_ref(url, local_path, db_cursor):
-	print("  Parsing " + local_path)
-	urllib.urlretrieve(url, local_path)
-	soup = BeautifulSoup(open(local_path).read())
+DOC_ZIP = "ref-doc.zip"
 
-	# remove stuff we don't want
-	for div in soup.findAll('div', 'navbar'):
-		div.extract()
-	for div in soup.findAll('div', 'span3'):
-		div.extract()
-	for footer in soup.findAll('footer'):
-		footer.extract()
+def get_defold_sha1():
+	info_url = "http://d.defold.com/stable/info.json"
+	info_file = urllib.urlopen(info_url)
+	info = json.loads(info_file.read())
+	info_file.close()
+	return info["sha1"]
 
-	for link in soup.find_all('a'):
-		href = str(link.get("href"))
-		if "#" in href:
-			function_name = href.split('#')[1]
-			if "." in function_name:
-				function_name = function_name.split(".")[1]
-			path = local_path.replace(documents_path + "/", "") + href
-			db_cursor.execute('INSERT OR IGNORE INTO searchIndex(name, type, path) VALUES (?,?,?)', (function_name, 'Function', path))
-			print("    Adding documentation for " + function_name)
+def get_ref_doc():
+	sha1 = get_defold_sha1()
+	if os.path.exists(DOC_ZIP):
+		os.remove(DOC_ZIP)
+	urllib.urlretrieve("http://d.defold.com/archive/" + sha1 + "/engine/share/ref-doc.zip", DOC_ZIP)
 
-	# update file with the changes made (removed divs etc)
-	with open(local_path, "wb") as file:
-		file.write(soup.prettify("utf-8"))
+def unzip_ref_doc(path):
+	if os.path.exists(path):
+		shutil.rmtree(path)
+
+	with zipfile.ZipFile(DOC_ZIP) as zf:
+		zf.extractall(path)
+
+def create_paths():
+	# create all paths
+	if os.path.exists(ref_path):
+		shutil.rmtree(ref_path)
+	os.makedirs(ref_path)
+
+	# create Info.plist
+	with open(os.path.join(contents_path, "Info.plist"), "w") as file:
+		file.write(INFO_PLIST)
+
+	# copy icon
+	shutil.copyfile("icon.png", os.path.join(docset_path, "icon.png"))
+
+get_ref_doc()
+
+JSON_PATH = "json"
+unzip_ref_doc(JSON_PATH)
+create_paths()
 
 
-# create all paths
-if os.path.exists(ref_path):
-	shutil.rmtree(ref_path)
-os.makedirs(ref_path)
-
-# create Info.plist
-with open(os.path.join(contents_path, "Info.plist"), "w") as file:
-	file.write(INFO_PLIST)
-
-# copy icon
-shutil.copyfile("icon.tiff", os.path.join(docset_path, "icon.tiff"))
 
 # create sqlite db
-with sqlite3.connect(db_path) as db:
+with sqlite3.connect(os.path.join(resources_path, "docSet.dsidx")) as db:
 	cursor = db.cursor()
 
 	try: cursor.execute('DROP TABLE searchIndex;')
 	except: pass
+
 	# create db table
 	cursor.execute('CREATE TABLE searchIndex(id INTEGER PRIMARY KEY, name TEXT, type TEXT, path TEXT);')
 	# make sure duplicates are ignored
 	#cursor.execute('CREATE UNIQUE INDEX anchor ON searchIndex (name, type, path);')
 
-	# scrape doc.defold.com, download index page and all /ref pages
-	urllib.urlretrieve("http://doc.defold.com", index_path)
-	soup = BeautifulSoup(open(index_path).read())
+	index_html = ""
+	for root, dir, files in os.walk(JSON_PATH):
+		for file in files:
+			with open(os.path.join(root, file), "r") as fh:
+				if file.endswith(".json"):
+					class_name = file.replace("_doc.json", "")
+					class_path = class_name + ".html"
+					class_doc = ""
+					for element in json.load(fh)["elements"]:
+						function_name = element["name"]
+						function_path = class_path + "#" + function_name
+						class_doc = class_doc + "<h1><a name='" + function_name + "'>" + function_name + "</a></h1>"
+						class_doc = class_doc + "<p>" + element["brief"] + "</p>"
+						if element["description"] != "":
+							class_doc = class_doc + "<p>" + element["description"] + "</p>"
+						if len(element["parameters"]) > 0:
+							class_doc = class_doc + "<h3>PARAMETERS</h3>"
+							for parameter in element["parameters"]:
+								class_doc = class_doc + "<p>" + parameter["name"] + " - "  + parameter["doc"] + "</p>"
+						if element["return_"] != "":
+							class_doc = class_doc + "<h3>RETURN</h3>"
+							class_doc = class_doc + "<p>" + element["return_"] + "</p>"
+						if element["examples"] != "":
+							class_doc = class_doc + "<h3>EXAMPLES</h3>"
+							class_doc = class_doc + "<p>" + element["examples"] + "</p>"
 
-	# remove stuff we don't want
-	for div in soup.findAll('div', 'navbar'):
-		div.extract()
-	for div in soup.findAll('div', 'page-header'):
-		div.extract()
-	for div in soup.findAll('div', 'ue-tab-container'):
-		div.extract()
-	for div in soup.findAll('div', 'well-blue'):
-		div.extract()
-	for div in soup.findAll('div', 'well-orange'):
-		div.extract()
-	for footer in soup.findAll('footer'):
-		footer.extract()
+						type = "Function"
+						if element["type"] == "VARIABLE":
+							type = "Variable"
+						elif element["type"] == "MESSAGE":
+							type = "Variable"
+						cursor.execute('INSERT OR IGNORE INTO searchIndex(name, type, path) VALUES (?,?,?)', (function_name, 'Function', "ref/" + function_path))
 
-	#urllib.urlretrieve("http://doc.defold.com/", index_path)
-	#soup = BeautifulSoup(open(index_path).read())
-	for link in soup.find_all('a'):
-		href = str(link.get("href"))
-		if "/ref/" in href:
-			class_name = href.split("/")[2]
-			class_path = "." + href + ".html"
-			print("Adding documentation for " + class_name + " from " + class_path)
-			cursor.execute('INSERT OR IGNORE INTO searchIndex(name, type, path) VALUES (?,?,?)', (class_name, 'Class', class_path))
-			parse_ref("http://doc.defold.com" + href, documents_path + href + ".html", cursor)
-			link["href"] = class_path
+					index_html = index_html + "<a href='ref/" + class_path + "'>" + class_name + "</a></br>"
+					with open(os.path.join(ref_path, class_path), "w") as out:
+						out.write(class_doc)
 
-	# write updated index file
-	with open(index_path, "wb") as file:
-		file.write(soup.prettify("utf-8"))
+					cursor.execute('INSERT OR IGNORE INTO searchIndex(name, type, path) VALUES (?,?,?)', (class_name, 'Module', "ref/" + class_path))
+
+	with open(os.path.join(documents_path, "index.html"), "w") as out:
+		out.write(index_html)
